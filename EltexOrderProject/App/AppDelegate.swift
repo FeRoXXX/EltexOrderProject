4122ab8b7eb28d0e72a0355a1d46be74b808e748
Александр Федоткин <ben115899@gmail.com>
2024-10-16 12:17:58 +0700

    Setup standart project

---
+++ EltexOrderProject/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  EltexOrderProject
+//
+//  Created by Александр Федоткин on 16.10.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ EltexOrderProject/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  EltexOrderProject
+//
+//  Created by Александр Федоткин on 16.10.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.makeKeyAndVisible()
+        window.rootViewController = ViewController()
+        self.window = window
+    }
+}
+
